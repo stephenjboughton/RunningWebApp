93bd9ff25fb3e5b0ac05c8121aa851c56aaf76ae
Stephen Boughton <stephenjboughton@gmail.com>
2018-08-02 22:37:50 -0400

    updated css with media queries to make it look pretty on mobile, updated style of input fields, created second html object in show history with abbreviated time display and used display none based on media querty added return to home buttons on all views

--- RunningWebApp/wwwroot/js/site.js
+++ RunningWebApp/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+/*
+document.addEventListener('DOMContentLoaded', () => {
+    // Code that runs when the DOM is loaded and verifies we have attached event handlers
+    console.log('DOM Loaded');
+    // Locate the time buttons (list items)
+    const historyRow = document.querySelectorAll('tr');
+    historyRow.forEach((historyRow) => {
+        // Attach an event handler for click on a list item
+        historyRow.addEventListener('click', (event) => {
+            console.log('history rows added');    
+            highlightRow(event.currentTarget);
+        });
+    });
+});
+
+function highlightRow(row) {
+    console.log('booyah');
+    row.classList.add('shine');
+}
+*/
