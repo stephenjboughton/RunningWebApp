4da5bc4f79b23e75f569e099e971898e10282398
Stephen <stephenjboughton@gmail.com>
2018-09-24 14:21:24 -0400

    converted list on index to sereis of anchors with button functionality, added mobile-sensitive link to runrecorder and added basic modal for rrunrecorder page

--- RunningWebApp/wwwroot/js/site.js
+++ RunningWebApp/wwwroot/js/site.js
@@ -24,7 +24,25 @@ function getElementFromTemplate(id) {
 document.addEventListener('DOMContentLoaded', () => {
     // Code that runs when the DOM is loaded and verifies we have attached event handlers
     console.log('DOM Loaded');
-    // Locate the try it button
+
+    // Get the modal
+    const modal = document.getElementById('recorder-warning');
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close-modal")[0];
+    // Make modal appear
+    modal.style.display = "block";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    // Locate the Start button
     const Start = document.querySelector('button#start');
     const CalculatePace = document.querySelector('button#submit-data');
     // Attach an event handler for a click on the button
@@ -116,7 +134,7 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(addWaypoint);
     } else {
-        x.innerText = "Geolocation is not supported by this browser.";
+        console.log('Geolocation is not supported by this browser.');
     }
 }
 
@@ -224,4 +242,33 @@ Number.prototype.pad = function (size) {
     var s = String(this);
     while (s.length < (size || 2)) { s = "0" + s; }
     return s;
+}
+
+
+
+
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
